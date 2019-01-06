194dc8ad407fb4da8710c0da935dd651053d034c
Adam Yi <i@adamyi.com>
2019-01-07 01:33:57 +1100

    update CI llvm to 7

--- ci_run.sh
+++ ci_run.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
+# CI script for the Fury of Dracula
+# written by Adam Yi <i@adamyi.com>
+
+CLANG_FORMAT="clang-format-7"
+if [ -z "$(which $CLANG_FORMAT)" ]; then
+  CLANG_FORMAT="clang-format"
+fi
+
+echo "Using clang-format: $CLANG_FORMAT"
+
+CLANG_TIDY="clang-tidy-7"
+if [ -z "$(which $CLANG_TIDY)" ]; then
+  CLANG_TIDY="clang-tidy"
+fi
+
+echo "Using clang-tidy: $CLANG_TIDY"
 
 set -e
 set -x
 
 make
 
-clang-format --version
-./run-clang-format.py *.c *.h
+$CLANG_FORMAT --version
+./run-clang-format.py --clang-format-executable $CLANG_FORMAT *.c *.h
 
-clang-tidy-4.0 --version
-clang-tidy-4.0 *.c *.h
+$CLANG_TIDY --version
+$CLANG_TIDY *.c *.h
 
 ./test_dracula_view
 ./test_game_view
