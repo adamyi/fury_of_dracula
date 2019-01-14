db2122d6f5cd3506ce9c9c9bdec9feeed7fac6a9
Adam Yi <i@adamyi.com>
2019-01-14 11:58:25 +1100

    fix coding style

--- dracula_view.c
+++ dracula_view.c
@@ -46,76 +46,76 @@ location_t dv_get_location(dracula_view *dv, enum player player) {
 }
 
 void dv_get_player_move(dracula_view *dv, enum player player, location_t *start,
-                        location_t *end) {//TODO TEST
+                        location_t *end) {  // TODO TEST
   *end = gv_get_location(dv->gv, player);
-  
+
   location_t trail[TRAIL_SIZE];
   gv_get_history(dv->gv, player, trail);
   round_t round = gv_get_round(dv->gv);
-  
+
   if (round == 0) {
     *start = UNKNOWN_LOCATION;
-    
+
   } else {
     *start = trail[0];
   }
   return;
 }
 
-void dv_get_locale_info(dracula_view *dv, location_t where, int *n_traps,
-                        int *n_vamps) {//TODO traps and vampire tracking can be done in gameview in second part of assignment
-//TODO TEST
+void dv_get_locale_info(
+    dracula_view *dv, location_t where, int *n_traps,
+    int *n_vamps) {  // TODO traps and vampire tracking can be done in gameview
+                     // in second part of assignment
+  // TODO TEST
   int last_hunter_visit = TRAIL_SIZE;
   enum player hunters = 0;
-  
+
   location_t trail[TRAIL_SIZE] = {-1};
-  
-  while (hunters <= PLAYER_MINA_HARKER){
+
+  while (hunters <= PLAYER_MINA_HARKER) {
     gv_get_history(dv->gv, hunters, trail);
     int i = 0;
-    while (i < TRAIL_SIZE){
+    while (i < TRAIL_SIZE) {
       if (trail[i] == where && i < last_hunter_visit) {
         last_hunter_visit = i;
         break;
       }
       i++;
     }
-    
   }
   int j;
-  if (last_hunter_visit == TRAIL_SIZE){
-    j = TRAIL_SIZE - 1;  
+  if (last_hunter_visit == TRAIL_SIZE) {
+    j = TRAIL_SIZE - 1;
   } else {
     j = last_hunter_visit;
   }
-  
+
   gv_get_history(dv->gv, PLAYER_DRACULA, trail);
-  
+
   while (j > 0) {
     if (trail[j] == where) {
-      if (get_round_trail_move_made(dv, j)%13 == 0) {
+      if (get_round_trail_move_made(dv, j) % 13 == 0) {
         *n_vamps++;
       } else {
         *n_traps++;
-      } 
+      }
     }
     j--;
   }
-  
-  
+
   return;
 }
-//function to take the index of a trail move and determine the round that move was made in
-static int get_round_trail_move_made(dracula_view *dv, int index){
+// function to take the index of a trail move and determine the round that move
+// was made in
+static int get_round_trail_move_made(dracula_view *dv, int index) {
   int round = gv_get_round(dv->gv);
-    return round - index;  
-  
+  return round - index;
 }
 
 void dv_get_trail(dracula_view *dv, enum player player,
                   location_t trail[TRAIL_SIZE]) {
   gv_get_history(dv->gv, player, trail);
-  //TODO check how doublebacks influence trails & test
+  // TODO check how doublebacks influence trails & test
 }
 
 location_t *dv_get_dests(dracula_view *dv, size_t *n_locations, bool road,
